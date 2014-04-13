aa76494ca0847a8cff86bd73f2dc11d054dff69a
Stephen Franklin <stephenLfranklin@gmail.com>
2014-04-12 22:14:47 -0500

    changed wd. made factors. working on split/colMeans

--- run_analysis.R
+++ run_analysis.R
@@ -59,56 +59,72 @@ if(!file.exists("UCI HAR Dataset")){
 # The features (columns) are listed in features.txt. There are 561.
 
 ### Get training data and features ###
-X_train <- read.table('train/X_train.txt') # training sample
+X_train <- read.table('UCI HAR Dataset/train/X_train.txt') # training sample
 View(X_train)
 dim(X_train)    # 7352  561
-features <- read.table('features.txt') # 2 561
-dim(features)    # 2 561
+features <- read.table('UCI HAR Dataset/features.txt') # 2 561
+dim(features)    # 561   2
 View(features)
 names(X_train) <- features$V2  # now we have named variables.
 
 # Get activity labels
-y_train <- read.table('train/y_train.txt')
-dim(y_train)    # [1] 7352    1
+y_train <- read.table('UCI HAR Dataset/train/y_train.txt')
+dim(y_train)    # 7352    1
 names(y_train) <- "activity"
 View(y_train)
 yX_train <- cbind(y_train,X_train)
 View(yX_train)
-activity_labels <- read.table('activity_labels.txt')
+activity_labels <- read.table('UCI HAR Dataset/activity_labels.txt')
 View(activity_labels)
-yX_train$activity <- mapvalues(yX_train$activity, activity_labels$V1, as.character(activity_labels$V2))
+activities <- factor(as.character(activity_labels$V2),levels = activity_labels$V2)  # as ordered factor
+is.factor(activities)   # TRUE
+levels(activities)
+yX_train$activity <- mapvalues(yX_train$activity, activity_labels$V1, as.character(activities))
+is.factor(yX_train$activity)  # FALSE
+    ## we want $activity to be a factor in the originally given order.
+yX_train$activity <- factor(yX_train$activity, levels=activities)
+levels(yX_train$activity)
 ## Now we have the training activities labeled.
 
 # Get training subjects
-subject_train<- read.table("train/subject_train.txt")
+subject_train<- read.table("UCI HAR Dataset/train/subject_train.txt")
 dim(subject_train)      # 7352 1
 names(subject_train) <- "subject"
 yX_train <- cbind(subject_train,yX_train)
 View(yX_train)
+yX_train$subject <- factor(yX_train$subject)
+is.factor(yX_train$subject)
+levels(yX_train$subject)
 
 ### Get testing data and features ###
-X_test <- read.table('test/X_test.txt') # training sample
+X_test <- read.table('UCI HAR Dataset/test/X_test.txt') # training sample
 View(X_test)
 dim(X_test)    # 2947  561
 names(X_test) <- features$V2  # now we have named variables.
 
 # Apply activity labels
-y_test <- read.table('test/y_test.txt')
+y_test <- read.table('UCI HAR Dataset/test/y_test.txt')
 dim(y_test)    # 2947    1
 names(y_test) <- "activity"
 View(y_test)
 yX_test <- cbind(y_test,X_test)
 View(yX_test)
-yX_test$activity <- mapvalues(yX_test$activity, activity_labels$V1, as.character(activity_labels$V2))
+yX_test$activity <- mapvalues(yX_test$activity, activity_labels$V1, as.character(activities))
+yX_test$activity <- factor(yX_test$activity, levels=activities)
+levels(yX_test$activity)
 ## Now we have the test activities labeled.
 
 # Get testing subjects
-subject_test<- read.table("test/subject_test.txt")
+subject_test<- read.table("UCI HAR Dataset/test/subject_test.txt")
 dim(subject_test)      # 2947    1
 names(subject_test) <- "subject"
 yX_test <- cbind(subject_test,yX_test)
 View(yX_test)
+yX_test$subject <- factor(yX_test$subject)
+is.factor(yX_test$subject)
+levels(yX_test$subject)
 # We now have well labeled training and test data.frames
+# And the subject and activity variables are factors.
 
 # Merge train and test sets
 yX <- rbind(yX_train,yX_test)
@@ -121,6 +137,14 @@ names(yX[201:300])
 i <- grep("(mean\\()|(std)", names(yX))
 Mean_Std <- yX[,c(1,2,i)]
 View(Mean_Std)
+levels(Mean_Std$subject)    # train [1:30] then test [2:24].
+                            # not sure that's what we want.
+levels(Mean_Std$activity)   # goood.
 
 # Create a second, independent tidy data set with the average 
 #   of each variable for each activity and each subject.
+ms999<-Mean_Std[1:999,]
+s<-split(ms999, list(ms999$subject,ms999$activity), drop=TRUE)
+letsee<-lapply(s, function(x) colMeans(x[3:68]))
+length(letsee)
+View(letsee)
